f873ed0e10bd759afeaafdfb76b185548b31fbaf
hashimotodesu <flammable53permission@gmail.com>
2021-05-03 10:42:11 +0900

    Listsindex.jsの編集

--- app/javascript/components/ListsIndex.js
+++ app/javascript/components/ListsIndex.js
@@ -1,10 +1,44 @@
 import React from "react"
 import PropTypes from "prop-types"
 class ListsIndex extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      lists: []
+    };
+  }
+  
+  componentDidMount(){
+    this.setState({
+      lists: this.props.lists
+    })
+  }
+
+
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.lists.map((list) => {
+              return (
+                <tr key={list.id}>
+                  <td>{list.id}</td>
+                  <td>{list.title}</td>
+                  <td>{list.description}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
